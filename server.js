d6ca22c3725d194b29f61436e012e763346593f9
pitronalldak <ryabinin.p.b@gmail.com>
2017-03-20 18:24:17 +0800

    changed Thank you language

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ bot.on('callback_query', callbackQuery => {
                                         bot.sendMessage(chatId, responseQuestion.question, opts)
                                     }
                                 } else {
-                                    bot.sendMessage(chatId, 'Thank you!');
+                                    bot.sendMessage(chatId, 'سپاس!');
                                 }
                             })
                     })
@@ -217,7 +217,7 @@ bot.on('message', msg => {
                                     bot.sendMessage(chatId, responseQuestion.question, opts)
                                 }
                             } else {
-                                bot.sendMessage(chatId, 'Thank you!');
+                                bot.sendMessage(chatId, 'سپاس!');
                             }
                         })
                 })
